bb734ccc49a1341c0b644c89f92f15e896f94200
Prashant <prashant.chourasia.09@gmail.com>
2024-12-14 17:23:26 +0530

    Implemented Complete and Exit button in the GUI

--- gui.py
+++ gui.py
@@ -6,8 +6,12 @@ input_box = sg.InputText(tooltip="Enter To-Do", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=Functions.read_todos(), key="todo_list", enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
-window = sg.Window("My To-Do App", layout=[[label],[input_box, add_button],[list_box],[edit_button]], font=("Helvetica",15))
+window = sg.Window("My To-Do App",
+                   layout=[[label],[input_box, add_button],[list_box],[edit_button, complete_button],[exit_button]],
+                   font=("Helvetica",15))
 
 while True:
     event, value = window.read()
@@ -31,6 +35,17 @@ while True:
             Functions.write_todos(edit_todo_list)
             window["todo_list"].update(values=edit_todo_list)
             window["todo"].update(value="")
+
+        case "Complete":
+            todo_complete = value["todo_list"][0]
+            todos = Functions.read_todos()
+            todos.remove(todo_complete)
+            Functions.write_todos(todos)
+            window["todo_list"].update(values=todos)
+            window["todo"].update(value="")
+
+        case "Exit":
+            break
 
         case "todo_list":
             selected_todo = value["todo_list"][0]
